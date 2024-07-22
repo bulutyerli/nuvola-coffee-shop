67fc7fb3b6b381fe5f48051a55f90e22ffa76f6c
bulut yerli <bulut@buluts-Mac-mini.local>
2024-07-23 00:05:54 +0300

    fix stripe success page, created layout

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -2,58 +2,26 @@
 
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import CheckoutForm from '@/src/components/CheckoutForm/CheckoutForm';
 import styles from './checkout.module.scss';
-import { useSelector } from '@/src/redux/store';
+import { RootState, useDispatch, useSelector } from '@/src/redux/store';
 import { Address } from '@/src/database-types';
 import Container from '@/src/components/Container/Container';
 import AddressCard from '@/src/components/AddressCard/AddressCard';
 import SmallProductCard from '@/src/components/SmallProductCard/SmallProductCard';
+import { selectAddress } from '@/src/redux/slices/addressSlice';
 
 // Make sure to call loadStripe outside of a component’s render to avoid
 // recreating the Stripe object on every render.
 // This is your test publishable API key.
 
 export default function CheckoutPage() {
-  const [clientSecret, setClientSecret] = useState('');
+  const dispatch = useDispatch();
   const { items, totalPrice, totalItems } = useSelector((state) => state.cart);
-  const [addresses, setAddresses] = useState<Address[] | []>([]);
-  const [selectedAddress, setSelectedAddress] = useState<Address | null>(null);
-  const [stripePromise, setStripePromise] = useState(() =>
-    loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
+  const { addresses, selectedAddress } = useSelector(
+    (state: RootState) => state.address
   );
-
-  useEffect(() => {
-    const getAddresses = async () => {
-      const res = await fetch('/api/addresses');
-      const result = await res.json();
-      setAddresses(result.addresses);
-
-      if (result.addresses.length > 0) {
-        setSelectedAddress(result.addresses[0]);
-      }
-    };
-
-    getAddresses();
-  }, []);
-
-  useEffect(() => {
-    // Create PaymentIntent as soon as the page loads
-    if (selectedAddress) {
-      fetch('/api/payment', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ items, address_id: selectedAddress?.id }),
-      })
-        .then((res) => res.json())
-        .then((data) => setClientSecret(data.clientSecret));
-    }
-  }, [selectedAddress]);
-
-  const options = {
-    clientSecret,
-  };
 
   const handleAddressChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedAddressId = event.target.value;
@@ -62,7 +30,7 @@ export default function CheckoutPage() {
     );
 
     if (address) {
-      setSelectedAddress(address);
+      dispatch(selectAddress(address));
     }
   };
 
@@ -90,18 +58,9 @@ export default function CheckoutPage() {
             {selectedAddress && <AddressCard address={selectedAddress} />}
           </section>
           <section className={styles.paymentSection}>
-            {clientSecret && (
-              <Elements
-                key={clientSecret}
-                options={options}
-                stripe={stripePromise}
-              >
-                <div className={styles.checkoutForm}>
-                  <CheckoutForm />
-                </div>
-                <section></section>
-              </Elements>
-            )}
+            <div className={styles.checkoutForm}>
+              <CheckoutForm />
+            </div>
           </section>
         </div>
         <section className={styles.cartSection}>
--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import cartSlice from './slices/cartSlice';
+import addressSlice from './slices/addressSlice';
 
 const rootReducer = combineReducers({
   cart: cartSlice,
+  address: addressSlice,
 });
 
 export default rootReducer;

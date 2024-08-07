0a381bc66ca4502b67fafa5f9da8e01a167d8dbb
Bulut Yerli <bulutyerli@gmail.com>
2024-08-07 15:57:24 +0300

    updated cognito

--- src/utils/authConfig.ts
+++ src/utils/authConfig.ts
@@ -4,6 +4,5 @@ export const authConfig: ResourcesConfig['Auth'] = {
   Cognito: {
     userPoolId: String(process.env.NEXT_PUBLIC_USER_POOL_ID),
     userPoolClientId: String(process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID),
-    identityPoolId: String(process.env.NEXT_PUBLIC_IDENTITY_POOL_ID),
   },
 };
